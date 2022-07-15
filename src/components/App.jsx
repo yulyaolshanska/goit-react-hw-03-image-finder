3862c8012417bea15fbe8ba60efa9b3a4feb4a31
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-15 19:29:23 +0300

    Fetch
    1. Аxios.
    2. FetchImage и изменение this.state.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,36 @@
 import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { fetchImage } from '../services/ApiServices';
 
 export class App extends Component {
-  state = {};
-
-  formData = data => {
-    console.log(data);
+  state = {
+    searchImg: '',
+    page: 1,
+    resolve: [],
   };
 
+  formData = data => {
+    this.setState({
+      searchImg: data,
+      page: 1,
+    });
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { searchImg, page } = this.state;
+    if (prevState.searchImg !== this.state.searchImg) {
+      const resolve = await fetchImage(searchImg, page);
+      this.setState({ resolve: resolve.data.hits });
+    }
+  }
+
   render() {
+    const { resolve } = this.state;
     return (
       <div>
         <Searchbar onSubmit={this.formData} />
-        <ImageGallery />
+        <ImageGallery images={resolve} />
       </div>
     );
   }
